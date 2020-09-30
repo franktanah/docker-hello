e7d783383145dfb4368d571e111e5fc4f10c3645
Frank Tan <franktanah@gmail.com>
2020-09-30 22:57:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
-cd /tmp
-echo "Fintech Class 2!<hr>Running on $(hostname)" > index.html
-python -m SimpleHTTPServer 8080
+echo Cleanup...
+docker kill mymongo 2>/dev/null
+docker rm mymongo 2>/dev/null
+
+echo Starting MongoDB
+docker run --name mymongo -d mongo
+echo Hang on for a moment...
+sleep 10
+echo Starting Mongo Shell
+docker exec -it mymongo mongo
+
+echo "MongoDB Shell stopped. Did you press Ctrl-C maybe? Anyway, simply run the $0 script again like previously."
